1886a03e2f0e0dd213c6cd9db256976d2ecedbda
adekmzrk <adekmzrk@gmail.com>
2022-01-23 15:26:48 +0700

    update

--- admin/footer.php
+++ admin/footer.php
@@ -1,6 +1,6 @@
 <br>
     <div class="footer">
-        <p><strong>THANKS COFFEE you make me specia; </strong> - Some right reserved 2022</p>
+        <p><strong>THANKS COFFEE you make me special ❤❤❤; </strong> - Some right reserved 2022</p>
       </div>
 </body>
 </html> 
