1986efdaa48cc4b631e9cd015bc771610bbd7b90
yehezikabeatrix@gmail.com <yehezikabeatrix>
2022-01-23 14:52:35 +0700

    update footer

--- admin/footer.php
+++ admin/footer.php
@@ -1,6 +1,6 @@
 <br>
     <div class="footer">
-        <p><strong>THANKS COFFEE</strong> - Some right reserved 2022</p>
+        <p><strong>THANKS COFFEE <3</strong> - Some right reserved 2022</p>
       </div>
 </body>
 </html>
